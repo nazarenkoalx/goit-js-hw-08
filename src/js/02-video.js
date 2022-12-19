497d840309d67d66e3edd3e1ac6835b054ff01b8
Nazarenko Alex <nazarenko.alx@gmail.com>
2022-12-19 22:22:42 +0200

    video improve

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,57 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(saveTime, 1000));
 
-// player.on('timeupdate', saveTime);
-
 setSavedTime();
 
+// variant 1
 function saveTime() {
   player.on('timeupdate', function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
   });
 }
 
 function setSavedTime() {
-  const videoSeconds = localStorage.getItem('videoplayer-current-time');
-  player.setCurrentTime(videoSeconds).catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        break;
+  try {
+    const savedData = JSON.parse(
+      localStorage.getItem('videoplayer-current-time')
+    );
 
-      default:
-        break;
-    }
-  });
+    player.setCurrentTime(savedData.seconds).catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+  } catch (error) {
+    error.name;
+    error.message;
+  }
 }
+
+// variant 2
+// function saveTime() {
+//   player.on('timeupdate', function (data) {
+//     localStorage.setItem('videoplayer-current-time', data.seconds);
+//   });
+// }
+
+// function setSavedTime() {
+// const videoSeconds = localStorage.getItem('videoplayer-current-time');
+//   player.setCurrentTime(videoSeconds).catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         break;
+
+//       default:
+//         break;
+//     }
+//   });
+// }
