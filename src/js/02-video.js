c01897bd0b0b7a1428c82ebdbdf4b53f33717963
Nazarenko Alex <nazarenko.alx@gmail.com>
2022-12-21 23:51:10 +0200

    update video-js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,49 +9,49 @@ player.on('timeupdate', throttle(saveTime, 1000));
 setSavedTime();
 
 // variant 1
-function saveTime() {
-  player.on('timeupdate', function (data) {
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
-  });
-}
-
-function setSavedTime() {
-  try {
-    const savedData = JSON.parse(
-      localStorage.getItem('videoplayer-current-time')
-    );
-
-    player.setCurrentTime(savedData.seconds).catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          break;
-
-        default:
-          break;
-      }
-    });
-  } catch (error) {
-    error.name;
-    error.message;
-  }
-}
-
-// variant 2
 // function saveTime() {
 //   player.on('timeupdate', function (data) {
-//     localStorage.setItem('videoplayer-current-time', data.seconds);
+//     localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
 //   });
 // }
 
 // function setSavedTime() {
-// const videoSeconds = localStorage.getItem('videoplayer-current-time');
-//   player.setCurrentTime(videoSeconds).catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         break;
+//   try {
+//     const savedData = JSON.parse(
+//       localStorage.getItem('videoplayer-current-time')
+//     );
 
-//       default:
-//         break;
-//     }
-//   });
+//     player.setCurrentTime(savedData.seconds).catch(function (error) {
+//       switch (error.name) {
+//         case 'RangeError':
+//           break;
+
+//         default:
+//           break;
+//       }
+//     });
+//   } catch (error) {
+//     error.name;
+//     error.message;
+//   }
 // }
+
+// variant 2
+function saveTime() {
+  player.on('timeupdate', function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  });
+}
+
+function setSavedTime() {
+  const videoSeconds = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(videoSeconds).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+}
